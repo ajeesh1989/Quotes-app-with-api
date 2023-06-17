b7a8d26e3cfdc5104ba33995187455f0e114d28b
ajeesh1989 <ajeeshrko@gmail.com>
2023-06-17 19:20:54 +0530

    completed

--- lib/about.dart
+++ lib/about.dart
@@ -7,77 +7,66 @@ class AboutApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-          centerTitle: true,
-          title: const Text('How to use this app?'),
-          backgroundColor: const Color.fromARGB(255, 14, 34, 44)),
-      body: SingleChildScrollView(
+        backgroundColor: Colors.teal,
+        centerTitle: true,
+        title: const Text('What is Quatify'),
+      ),
+      body: const SingleChildScrollView(
         child: Column(
           children: [
-            const Padding(
+            Padding(
               padding: EdgeInsets.all(8.0),
               child: Text(
                 textAlign: TextAlign.justify,
-                "An EMI (Equated Monthly Installment) calculator is a useful tool that helps individuals and businesses determine the amount they need to pay each month towards a loan or mortgage.",
+                "Quotify is a quote app that allows users to explore and engage with a vast collection of inspiring quotes. With Quotify, users can discover meaningful quotes, copy them for personal use, add them to their favorites, and share them on social media platforms. The app offers a user-friendly interface and a seamless experience for quote enthusiasts.",
                 style: TextStyle(fontSize: 16),
               ),
             ),
-            const Padding(
+            Padding(
               padding: EdgeInsets.all(8.0),
               child: Text(
                   textAlign: TextAlign.justify,
-                  "1.An EMI calculator is a financial tool designed to calculate the Equated Monthly Installment, which is the fixed amount borrowers need to pay towards a loan or mortgage.",
+                  "1.Explore Quotes: Quotify provides a diverse range of quotes, including love, life,friendship,success,happiness etc and thought-provoking content. Users can browse through a vast collection of quotes from various categories.",
                   style: TextStyle(fontSize: 15)),
             ),
-            const Padding(
+            Padding(
               padding: EdgeInsets.all(8.0),
               child: Text(
                 textAlign: TextAlign.justify,
-                "2.By using an EMI calculator, borrowers can easily determine the affordability of a loan and plan their finances accordingly.",
+                "2.Favorites: Quotify allows users to create a personalized collection of favorite quotes. By adding quotes to their favorites, users can easily revisit and reflect on the quotes that resonate with them the most.",
                 style: TextStyle(fontSize: 15),
               ),
             ),
-            const Padding(
+            Padding(
               padding: EdgeInsets.all(8.0),
               child: Text(
                 textAlign: TextAlign.justify,
-                "3.It takes into account factors such as the principal amount, interest rate, and loan tenure to calculate the EMI accurately.",
+                "3.Social Sharing: Users can share quotes directly from the app to their favorite social media platforms, such as Facebook, Twitter, Instagram, or WhatsApp. This feature enables users to spread inspiration and thought-provoking content with their friends and followers.",
                 style: TextStyle(
                   fontSize: 15,
                 ),
               ),
-            ),
-            const Padding(
-              padding: EdgeInsets.all(8.0),
-              child: Text(
-                textAlign: TextAlign.center,
-                "How to use:",
-                style: TextStyle(fontSize: 16, fontWeight: FontWeight.w500),
-              ),
-            ),
-            const Padding(
-              padding: EdgeInsets.all(8.0),
-              child: Text(
-                textAlign: TextAlign.justify,
-                "All you have to do is enter your principal amount, rate of interest and loan tenure in months. When you click on 'calculate EMI' button you will get Monthly EMI, Total interest and Total payment with graphical representation also.",
-                style: TextStyle(
-                  fontSize: 15,
-                ),
-              ),
-            ),
-            const SizedBox(
-              height: 15,
             ),
             Padding(
-              padding: const EdgeInsets.all(15.0),
+              padding: EdgeInsets.all(8.0),
               child: Text(
                 textAlign: TextAlign.justify,
-                "Remember, an EMI calculator is a valuable tool, but it's always recommended to consult with a financial advisor or lender for accurate and personalized loan calculations and advice.",
+                "4.Personalization: Quotify allows users to customize their experience by choosing their preferred quote categories or authors. This feature ensures that users receive quotes that align with their interests and preferences.",
                 style: TextStyle(
                   fontSize: 15,
-                  color: Colors.red.shade600,
                 ),
               ),
-            )
+            ),
+            Padding(
+              padding: EdgeInsets.all(8.0),
+              child: Text(
+                textAlign: TextAlign.justify,
+                "5.Copy Quotes: With a simple tap, users can easily copy their favorite quotes to their device's clipboard. This feature enables users to save quotes for personal use, such as adding them to notes or sharing them via messaging apps.",
+                style: TextStyle(
+                  fontSize: 15,
+                ),
+              ),
+            ),
           ],
         ),
       ),
